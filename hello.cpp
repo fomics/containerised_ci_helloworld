1f155d03adaea789fa76801957e0d92540098ecf
William Sawyer <wsawyer@cscs.ch>
2023-02-10 10:59:45 +0100

    Fourth change

--- hello.cpp
+++ hello.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
         std::cerr << "Error getting hostname" << std::endl;
         return 1;
     }
-    std::cout << "Even newer hello world from " << hostname << std::endl;
+    std::cout << "Newest hello world from " << hostname << std::endl;
 
     return 0;
 }
